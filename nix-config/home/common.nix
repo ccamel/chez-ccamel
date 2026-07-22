5280e522e39a87714a7e0006b6819c3ba95dc076
ccamel <camel.christophe@gmail.com>
2026-07-22 17:32:15 +0200

    :sparkles: Configure vital terminal tool integrations

--- nix-config/home/common.nix
+++ nix-config/home/common.nix
@@ -6,6 +6,31 @@
       nix-direnv.enable = true;
     };
 
+    bat = {
+      enable = true;
+      config = {
+        pager = "less";
+        italic-text = "always";
+        map-syntax = ".ignore:.gitignore";
+      };
+    };
+
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    delta = {
+      enable = true;
+      enableGitIntegration = true;
+    };
+
+
     git = {
       enable = true;
       lfs.enable = true;
@@ -15,10 +40,24 @@
       };
     };
 
+    gh = {
+      enable = true;
+      settings = {
+        git_protocol = "ssh";
+        editor = "vim";
+        prompt = "enabled";
+        pager = "bat";
+        aliases.co = "pr checkout";
+      };
+    };
+
+
     zsh = {
       enable = true;
       enableCompletion = true;
       autosuggestion.enable = true;
+      shellAliases.cat = "bat --style=plain --paging=never";
+
       oh-my-zsh = {
         enable = true;
         theme = "mh";
@@ -36,6 +75,30 @@
   };
 
   home.packages = with pkgs; [
+    # Search and navigation
+    ripgrep
+    fd
+
+    # Files and output
+    eza
+    tree
+    file
+
+    # Git
+    tig
+
+    # Data and HTTP
+    jq
+    yq-go
+    curl
+    wget
+
+    # System
+    btop
+    dust
+    procs
+    which
+
     age
     gnupg
     sops
