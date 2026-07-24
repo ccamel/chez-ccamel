7053758a596b5035d43dd9105df009e60791f190
ccamel <camel.christophe@gmail.com>
2026-07-25 00:06:15 +0200

    :wrench: Add ghostty config

--- nix-config/hosts/tinymac/home.nix
+++ nix-config/hosts/tinymac/home.nix
@@ -18,4 +18,31 @@
   home.packages = [ config.programs.home-manager.package ];
 
   home.file.".gitconfig".source = config.xdg.configFile."git/config".source;
+
+  xdg.configFile."ghostty/config".text = ''
+    # THEME SETTINGS
+    theme = iTerm2 Tango Dark
+    cursor-style = block
+    cursor-style-blink = true
+    shell-integration-features = no-cursor
+
+    # FONT SETTINGS
+    font-family = ProggyClean Nerd Font Mono
+    font-size = 19
+
+    # WINDOW SETTINGS
+    window-padding-balance = true
+    window-save-state = always
+    window-colorspace = "display-p3"
+    adjust-cell-height = 35%
+    link-url = true
+    macos-titlebar-style = tabs
+
+    # MOUSE
+    mouse-hide-while-typing = true
+    mouse-scroll-multiplier = 2
+
+    # SPLITS
+    unfocused-split-opacity = 0.5
+  '';
 }
