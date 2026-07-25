abe61558dfa14effdae5474b33bd94eb22396795
ccamel <camel.christophe@gmail.com>
2026-07-25 12:07:59 +0200

    :truck: Move Ghostty config into dedicated module

--- nix-config/hosts/tinymac/home.nix
+++ nix-config/hosts/tinymac/home.nix
@@ -6,6 +6,7 @@
     ../../home/modules/git-signing.nix
     ../../home/modules/git-mine.nix
     inputs.nix-index-database.homeModules.nix-index
+    ../../home/modules/ghostty
   ];
 
   home = {
@@ -21,30 +22,4 @@
 
   home.file.".gitconfig".source = config.xdg.configFile."git/config".source;
 
-  xdg.configFile."ghostty/config".text = ''
-    # THEME SETTINGS
-    theme = iTerm2 Tango Dark
-    cursor-style = block
-    cursor-style-blink = true
-    shell-integration-features = no-cursor
-
-    # FONT SETTINGS
-    font-family = ProggyClean Nerd Font Mono
-    font-size = 19
-
-    # WINDOW SETTINGS
-    window-padding-balance = true
-    window-save-state = always
-    window-colorspace = "display-p3"
-    adjust-cell-height = 35%
-    link-url = true
-    macos-titlebar-style = tabs
-
-    # MOUSE
-    mouse-hide-while-typing = true
-    mouse-scroll-multiplier = 2
-
-    # SPLITS
-    unfocused-split-opacity = 0.5
-  '';
 }
