4f58b5ec85f3ef9a9cec2a2bf9ab96b66f56e448
ccamel <camel.christophe@gmail.com>
2026-07-29 09:46:20 +0000

    :wrench: Enable weekly Docker auto-pruning

--- nix-config/hosts/forge/nixos.nix
+++ nix-config/hosts/forge/nixos.nix
@@ -5,6 +5,10 @@
   virtualisation.docker = {
     enable = true;
     package = pkgs.docker_29;
+    autoPrune = {
+      enable = true;
+      dates = "weekly";
+    };
   };
 
   networking.hostName = "forge";
