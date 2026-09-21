d3917aa2c317a7bd54b9e6dd81d5dfaf54e1ae20
ccamel <9574336+ccamel@users.noreply.github.com>
2026-09-21 09:57:57 +0000

    :arrow_up: Update fast dependencies

--- nix-config/packages/omp.nix
+++ nix-config/packages/omp.nix
@@ -7,18 +7,18 @@
 }:
 let
   # managed by update-resource
-  version = "18.2.6";
+  version = "18.2.7";
   source =
     {
       x86_64-linux = {
         url = "https://github.com/can1357/oh-my-pi/releases/download/v${version}/omp-linux-x64";
         # managed by update-resource
-        hash = "sha256-DzhZjJHoI9jM4H8VHsOZnVHyE/ssw+B9ifGvju+SR6I=";
+        hash = "sha256-HCjtZlZuyrq4FFFM/mJrSv9ZgbfKmJJHe0ZVD+a22Ds=";
       };
       aarch64-darwin = {
         url = "https://github.com/can1357/oh-my-pi/releases/download/v${version}/omp-darwin-arm64";
         # managed by update-resource
-        hash = "sha256-1JjaQNV34f+mgcqGMsLqQKn3IqCLiAQSAR033/7pUTo=";
+        hash = "sha256-Ky/yCEo09hoBhjTAOSmqqRFwCdEH2+9VPY/FHrXCixQ=";
       };
     }
     .${stdenvNoCC.hostPlatform.system}
