f29d6fc2c1b3136f159864317cdee8e52808628f
ccamel <camel.christophe@gmail.com>
2026-09-23 07:26:29 +0000

    :bug: Build sops-install-secrets with Go 1.26 module builder

--- nix-config/home/modules/sops.nix
+++ nix-config/home/modules/sops.nix
@@ -1,7 +1,15 @@
-{ config, ... }:
+{
+  config,
+  inputs,
+  pkgs,
+  ...
+}:
 {
   sops = {
     age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
     defaultSopsFormat = "yaml";
+    package = (pkgs.callPackage inputs.sops-nix { }).sops-install-secrets.override {
+      buildGoModule = pkgs.buildGo126Module;
+    };
   };
 }
